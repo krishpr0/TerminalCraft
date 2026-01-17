ebf682fad4bff5dc9a0549999e21fba9a73ec643
krishpr0 <krishnpradhan007@gmail.com>
2026-01-17 20:23:32 +0545

    Added static main menu layout

--- Tracking.c
+++ Tracking.c
@@ -1,10 +1,13 @@
 #include <ncurses.h>
 
+#define TITLE_Y 3
+
 int main(void)
 {
     initscr();
 
-    if (has_colors() == FALSE) {
+    if (has_colors() == FALSE)
+    {
         endwin();
         printf("Your terminal does not support color\n");
         return 1;
@@ -12,15 +15,21 @@ int main(void)
 
     start_color();
     use_default_colors();
-
-    init_pair(1, COLOR_RED, -1);
+    init_pair(1, COLOR_CYAN, -1);
+    init_pair(2, COLOR_WHITE, COLOR_BLUE);
 
     WINDOW *win = newwin(LINES, COLS, 0, 0);
+    keypad(win, TRUE);
     box(win, 0, 0);
 
     wattron(win, COLOR_PAIR(1));
     mvwprintw(win, 2, 4, "Budeget Tracker  version 1.0");
     wattroff(win, COLOR_PAIR(1));
+
+    mvwprintw(win, TITLE_Y + 3, (COLS - 30) / 2, "1. Add Expense");
+    mvwprintw(win, TITLE_Y + 4, (COLS - 30) / 2, "2. View Expenses");
+    mvwprintw(win, TITLE_Y + 5, (COLS - 30) / 2, "3. Generate Report");
+    mvwprintw(win, TITLE_Y + 6, (COLS - 30) / 2, "4. Exit");
 
     wresfresh(win);
     getch();
